cc702738ac1474311c2363ef550f2467d7841942
Ivan Martić <ivan.martic.ri@gmail.com>
2017-04-26 21:31:21 +0200

    modal update (...)

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -23,31 +23,27 @@
         </q-tooltip>
     </button>
 
-    <q-modal ref="infoModal" class="maximized">
+    <q-modal ref="infoModal" class="full-modal maximized">
         <q-layout>
-            <div slot="header" class="toolbar">
-                <button @click="$refs.layoutModal.close()">
-                    <i>keyboard_arrow_left</i>
-                </button>
-                <q-toolbar-title :padding="1">
-                    Header
-                </q-toolbar-title>
-            </div>
-                <div slot="header" class="toolbar primary">
-                <q-search class="primary"></q-search>
-            </div>
-            <div slot="footer" class="toolbar">
-                <q-toolbar-title :padding="1">
-                    Footer
-                </q-toolbar-title>
-            </div>
+
             <div class="layout-view">
-                <div class="layout-padding">
-                    <h1>Modal</h1>
-                    <button class="primary" @click="$refs.infoModal.close()">Close</button>
-                    <p class="caption" v-for="n in 15">This is a Modal presenting a Layout.</p>
+                <button class="close-modal" @click="$refs.infoModal.close()">
+                    <i>close</i>
+                </button>
+                <div class="layout-padding text-center">
+                    <h4 class="modal-title">Contact information</h4>
+                    <ul class="contact-info" v-for="item in contactInfo">
+                        <li>
+                            <a v-if="item.link !== null" :href="item.link">
+                                <span class="title" v-if="item.fa == null"><i>{{item.icon}}</i></span>
+                                <span class="title" v-else><i :class="item.icon"></i></span>
+                                <span class="text">{{item.text}}</span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+
         </q-layout>
     </q-modal>
 
@@ -63,6 +59,14 @@ export default {
         {text: 'About', link: '/about'},
         {text: 'Portfolio', link: '/portfolio'},
         {text: 'Contact', link: '/contact'}
+      ],
+      contactInfo: [
+        {text: 'ivan.martic.ri@gmail.com', icon: 'mail', link: 'mailto:ivan.martic.ri@gmail.com?Subject=Hello!'},
+        {text: 'imartic.github.io', icon: 'language', link: 'https://imartic.github.io'},
+        {text: '+385 91 200 8778', icon: 'phone'},
+        {text: 'imartic14', icon: 'fa fa-skype', fa: 'sykpe'},
+        {text: 'LinkedIn profile', icon: 'fa fa-linkedin', fa: 'linkedin', link: 'https://www.linkedin.com/in/ivan-martić-81b7a9a5'},
+        {text: 'GitHub profile', icon: 'fa fa-github', fa: 'github', link: 'https://www.github.com/imartic'}
       ]
     }
   }
@@ -106,4 +110,40 @@ export default {
 .contact-info-btn i{
     font-size: 26px;
 }
+
+.full-modal{
+    opacity: 0.9;
+    filter: alpha(opacity=90);
+}
+.full-modal .layout-view{
+    background-color: #14151a;
+}
+
+.close-modal i{
+    font-size: 30px
+}
+.close-modal {
+    color: #bbb;
+    position: absolute;
+    top: 15px;
+    right: 15px
+}
+.close-modal:hover {
+    color: #eee !important;
+}
+
+.modal-title{
+    font-weight: 300;
+    margin-top: 70px;
+    margin-bottom: 50px
+}
+
+.contact-info{list-style-type: none;padding:0}
+.contact-info li{padding-bottom:8px}
+.contact-info .title{color:#fff;display:inline-flex;vertical-align:middle;padding-bottom:4px}
+.contact-info .title i{font-size:23px;padding-right:5px}
+.contact-info .title .fa{padding-left:2px;}
+.contact-info .text{font-size:16px;font-weight:400;line-height:20px;color:#fff;}
+i{font-size:23px;padding-right:5px}
+.fa{padding-left:3px;}
 </style>
